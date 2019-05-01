448022745e8eecd5792fe295ae7630f6e9cc8a93
Kevin Dai <daikevin19@yahoo.com>
2019-05-01 15:49:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.size >= 1
+    newarray = []
+    counter = 1 
+    katz_deli.each do |name|
+      newarray.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{newarray.join(" ")}" ##" " is to add spaces between each person
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
